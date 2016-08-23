ad78274e3e95c54f779193eea818e047f523f391
= <aranlong@live.co.uk>
2016-08-23 21:12:03 +0100

    parameter corrections, docstring formatting fixes, constants capitalized, helper functions for a tag generation ...

--- scripts/pyblog.py
+++ scripts/pyblog.py
@@ -3,96 +3,102 @@ import time
 import glob
 import markdown
 
-template_url = "../web/templates"
-post_template_url = "../web/templates/post.html"
-contents_template_url = "../web/templates/contents.html"
+TEMPLATE_URL = "../web/templates"
+POST_TEMPLATE_URL = "../web/templates/post.html"
+CONTENTS_TEMPLATE_URL = "../web/templates/contents.html"
 
-posts_html_url = "../web"
-posts_markdown_url = "../markdown"
+POSTS_HTML_URL = "../web"
+POSTS_MARKDOWN_URL = "../markdown"
 
 def markdown_to_html(markdown_file_url, post_directory_url):
-    """ Convert a markdown file to html.
-    """
+    """ Convert a markdown file to html."""
 
-    print "Converting markdown file: " + os.path.basename(markdown_file_url) + " to html..."
+    print("Converting markdown file: " + os.path.basename(markdown_file_url) + " to html...")
 
     with open(markdown_file_url) as markdown_file:
         markdown_text = markdown_file.read()
 
 	html = markdown.markdown(markdown_text)
 
-	new_post_url = posts_html_url + "/" + os.path.basename(markdown_file_url).replace(".md", ".html")
+	new_post_url = post_directory_url + "/" + file_name(markdown_file_url) + ".html";
 
-	with open(post_template_url) as post_template_html_file:
+	with open(POST_TEMPLATE_URL) as post_template_html_file:
 		new_post_html = post_template_html_file.read()
 
-	new_post_html = new_post_html.replace("pyblog-title", os.path.basename(markdown_file_url).replace(".md", ""))
-	new_post_html = new_post_html.replace("pyblog-body", html)
+	new_post_html = new_post_html.replace("{{pyblog-title}}", file_name(markdown_file_url))
+	new_post_html = new_post_html.replace("{{pyblog-body}}", html)
 
 	with open(new_post_url, 'w') as new_post_html_file:
 		new_post_html_file.write(new_post_html)
 
-def generate_contents(post_directory_url):
-	""" Generate an blog page from a directory of html files.
-	"""
+def html_link(url, contents):
+	""" Return an 'a' html tag with href 'url' and contents 'contents'"""
 
-	print "Generating blog page..."
+	return "<a href =\"" + url + "\">" + contents + "</a>"; 
+
+def file_name(path):
+	""" Return a file name (excluding extension) from a file path"""
+
+	return os.path.splitext(os.path.basename(path))[0];
+
+def generate_contents(post_directory_url):
+	""" Generate an blog page from a directory of html files."""
+
+	print("Generating blog page...")
 
 	contents_url = post_directory_url + "/contents.html"
 
-	with open(contents_template_url) as contents_template_file:
+	with open(CONTENTS_TEMPLATE_URL) as contents_template_file:
 		new_contents_html = contents_template_file.read()
 
-	post_urls = glob.glob(posts_html_url + "/*.html")
+	post_urls = glob.glob(POSTS_HTML_URL + "/*.html")
 
 	contents_html_body = ""
 
 	for post_url in post_urls:
 		if "contents.html" not in post_url:
-			contents_html_body += "<a href = \"" + post_url.replace("/posts", "") + "\">" + os.path.basename(post_url).replace(".html", "") + "</a><br>\n"
+			contents_html_body += html_link(post_url, file_name(post_url)) + "<br>";
 
-	new_contents_html = new_contents_html.replace("pyblog-body", contents_html_body)
+	new_contents_html = new_contents_html.replace("{{pyblog-body}}", contents_html_body)
 
 	with open(contents_url, 'w') as new_contents_file:
 		new_contents_file.write(new_contents_html)
 
 def update_loop():
-	""" Check for file changes, reconvert to html.
-	"""
+	""" Check for file changes, reconvert to html. """
 
 	while True:
-		markdown_file_urls = glob.glob(posts_markdown_url + "/*.md")
-		template_file_urls = glob.glob(template_url + "/*")
+		markdown_file_urls = glob.glob(POSTS_MARKDOWN_URL + "/*.md")
+		template_file_urls = glob.glob(TEMPLATE_URL + "/*")
 
 		for markdown_file_url in markdown_file_urls:
 			last_modified_time = os.stat(markdown_file_url).st_mtime
 
 			if time.time() - last_modified_time < 5:
-				print "Post: " + markdown_file_url + " updated, converting to html..."
-				markdown_to_html(markdown_file_url, posts_html_url)
-				generate_contents(posts_html_url)
+				print("Post: " + markdown_file_url + " updated, converting to html...")
+				markdown_to_html(markdown_file_url, POSTS_HTML_URL)
+				generate_contents(POSTS_HTML_URL)
 
 		for template_file_url in template_file_urls:
 			last_modified_time = os.stat(template_file_url).st_mtime
 
 			if time.time() - last_modified_time < 5:
-				print "Template: " + template_file_url + " updated"
+				print("Template: " + template_file_url + " updated")
 				update_all()
 
 		time.sleep(5)
 
 
 def update_all():
-	""" Convert all markdown files to html, generate blog file.
-	"""
+	""" Convert all markdown files to html, generate blog file."""
 
-	print "Updating all posts..."
-	post_urls = glob.glob(posts_markdown_url + "/*.md")
+	print("Updating all posts...")
+	post_urls = glob.glob(POSTS_MARKDOWN_URL + "/*.md")
 
 	for post_url in post_urls:
-		markdown_to_html(post_url, posts_html_url)
+		markdown_to_html(post_url, POSTS_HTML_URL)
 
-	generate_contents(posts_html_url)
+	generate_contents(POSTS_HTML_URL)
 
 update_all()
 update_loop()
